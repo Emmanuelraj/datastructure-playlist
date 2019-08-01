ff1c403f9182e7dbaa86fbd8508773402b36cf3a
Emmanuel Raj <immanuel.raj18@gmail.com>
2019-08-01 17:57:13 +0530

    Update ReverseLinkedList.java

--- src/com/project/dailycoding/ReverseLinkedList.java
+++ src/com/project/dailycoding/ReverseLinkedList.java
@@ -19,6 +19,10 @@ Given a singly-linked list, reverse the list. This can be done iteratively or re
 Example:
 Input: 4 -> 3 -> 2 -> 1 -> 0 -> NULL
 Output: 0 -> 1 -> 2 -> 3 -> 4 -> NULL
+
+working
+run one method after another comments
+ 
  */
 
 
